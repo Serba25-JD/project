55297f20227097ecaa445012785517f94bf8db86
Serba25-JD <hydebb.99@gmail.com>
2025-11-21 12:07:48 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.createElement('header');
+    const logo = new Image();
+    logo.src = 'https://smk-katolik-mariana.infinityfreeapp.com/assets/image/SMK.png';
+    logo.width = '150';
+    logo.height = '123.56';
+    logo.loading = 'lazy';
+    const h1 = document.createElement('h1');
+    h1.textContent = 'SMK Swasta Katolik Mariana';
+    header.append(logo, h1);
+    const main = document.createElement('main');
+    const section = document.createElement('section');
+    const data = [
+        { 'label': 'Email', 'type': 'text', 'id': 'email'},
+        { 'label': 'Password', 'type': 'password', 'id': 'password'}
+    ];
+    data.forEach(input => {
+        const labelType = document.createElement('label');
+        labelType.textContent = input.label;
+        labelType.htmlFor = input.id;
+        const inputType = document.createElement('input');
+        inputType.type = input.type;
+        inputType.placeholder = `masukkan ${input.id}`;
+        inputType.name = input.id;
+        inputType.setAttribute('id', input.id);
+        section.append(labelType, inputType);
+    });
+    const button = document.createElement('button');
+    button.type = 'submit';
+    button.textContent = 'Login';
+    button.setAttribute('id', 'login-btn');
+    section.appendChild(button);
+    main.appendChild(section);
+    const footer = document.createElement('footer');
+    const pFooter = document.createElement('p');
+    pFooter.textContent = 'Copyright Yoshua Franly Sihombing @2025';
+    footer.appendChild(pFooter);
+    document.body.append(header, main, footer);
+    validateForm();
+});
+
+function validateForm() {
+    const button = document.getElementById('login-btn');
+    button.addEventListener('click', function() {
+        let error = document.getElementById('login-error');
+        if(!error) {
+            error = document.createElement('p');
+            error.setAttribute('id', 'login-error');
+            button.insertAdjacentElement('afterend', error);
+        };
+        const email = document.getElementById('email').value;
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        const password = document.getElementById('password').value;
+        if(!email && !password) {
+            error.textContent = 'Silahkan masukkan email dan password terlebih dahulu.';
+        } else if(!email) {
+            error.textContent = 'Silahkan masukkan email terlebih dahulu.';
+        } else if(!emailPattern.test(email)) {
+            error.textContent = 'Silahkan masukkan email dengan berformat @gmail.com';
+        } else if(!password) {
+            error.textContent = 'Silahkan masukkan password terlebih dahulu.';
+        } else {
+            error.remove();
+            showLoading();
+        };
+    });
+};
+
+function showLoading() {
+    const section = document.querySelector('section');
+    const loadingContainer = document.createElement('div');
+    loadingContainer.classList.add('loader-wrapper');
+    loadingContainer.setAttribute('id', 'loader');
+    const loadingContent = document.createElement('div');
+    loadingContent.classList.add('loader');
+    loadingContainer.appendChild(loadingContent);
+    section.insertAdjacentElement('afterend', loadingContainer);
+    showForm();
+};
+
+function showForm() {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const loader = document.getElementById('loader');
+    if(email.value && password.value) {
+        setTimeout(() => {
+            loader.remove();
+            setTimeout(() => {
+                alert('Login sukses.');
+                email.value = '';
+                password.value = '';
+            }, 50);
+        }, 2000);
+    };
+};
